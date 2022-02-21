e21d4378374c77ae8d20eff79d75b842b2f6888f
Neveen97 <neveenaburomman1@gmail.com>
2022-02-21 14:10:40 +0200

    class1

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+function start(port) {
+    app.listen(port,()=>{
+        console.log(`running on port ${port}`)
+    })
+}
+
+app.get('/',(req,res)=>{
+    res.send('home route')
+})
+
+app.get('/data',(req,res)=>{
+    res.status(200).json({
+        name: 'neveen',
+        email:'neveenaburomman1@gmail.com'
+    })
+})
+
+module.exports = {
+    app: app,
+    start: start
+}
